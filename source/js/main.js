d01b668c041f63f42fc75ef031e36be75b5d0e6a
AndersonTheThird <46432997+AndersonTheThird@users.noreply.github.com>
2020-04-14 15:19:19 -0400

    Update main.js

--- source/js/main.js
+++ source/js/main.js
@@ -297,28 +297,6 @@ function StartVMWebsocket(filename) {
     });
 }
 
-// C++ VM
-
-let add, divide, multiply, subtract;
-
-StartVMWebsocket(
-  "https://cdn.glitch.com/197f19a4-c913-4eb7-baf5-c1f36a4965be%2Ftest%20(4).wasm?v=1586730773116"
-).then(instance => {
-  add = instance.exports._Z3addii;
-  divide = instance.exports._Z6divideii;
-  multiply = instance.exports._Z8multiplyii;
-  subtract = instance.exports._Z8subtractii;
-});
-
-// C VM
-let letfunc;
-
-StartVMWebsocket(
-  "https://cdn.glitch.com/197f19a4-c913-4eb7-baf5-c1f36a4965be%2Ftest%20(5).wasm"
-).then(instance => {
-  letfunc = instance.exports.letfunc;
-});
-
 // Do a barrel roll easter egg
 const source = document.getElementById("search-input");
 
