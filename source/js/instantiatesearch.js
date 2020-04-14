d0f5915523b29b8d6fd5f365a7ff3062f659244b
AndersonTheThird <46432997+AndersonTheThird@users.noreply.github.com>
2020-04-14 19:16:27 -0400

    Update instantiatesearch.js

--- source/js/instantiatesearch.js
+++ source/js/instantiatesearch.js
@@ -1,10 +1,3 @@
-SimpleJekyllSearch({
-  searchInput: document.getElementById('search-input'),
-  resultsContainer: document.getElementById('searchcontent'),
-  json: './search.json',
-  searchResultTemplate: '<article style="border-radius: 20px;">                <a class="noline">                    <center><img style="border-radius: 20px; border-bottom-right-radius: 0px; border-bottom-left-radius: 0px;" src="{featimg}" alt="{{ thumb }}"></center>                </a>    <a>        <h1 style="text-decoration: none; cursor: text;">{title}</h1>    </a>    <h1 style="    font-size: 15px;    margin-top: -20px;    text-decoration: none;    ">Developed By <a href="javascript:soon();">{author}</a></h1><p class="post-content">{description}</p>    <p class="actions">       <a onclick="playgame(`{embed}`)"><i class="material-icons" style="    font-size: 30px;    text-decoration: none;    color: crimson;">play_arrow</i></a>    </p></article>'
-});
-// new
 const $source = document.getElementById("search-input");
 
 const typeHandler = function(e) {
