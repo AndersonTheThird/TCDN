e0483464cb948479edb1306186aed8570b32e9ec
AndersonTheThird <46432997+AndersonTheThird@users.noreply.github.com>
2020-05-10 23:20:49 -0400

    Update loadwasm.js

--- source/js/loadwasm.js
+++ source/js/loadwasm.js
@@ -1,22 +1,23 @@
 // WebAssebly - C Setup
 
-const WebSocketReady = false
+const FlagEnabled = false // Experimental Feature - Flag # 582343814
 
-if (WebSocketReady) {
+if (FlagEnabled) { // that stuff aint ready aw naw
   (async() => {
-const codePromise = fetch("https://cdn.tetretalk.com/source/wasm/output.wasm")
-const { instance } = await WebAssembly.instantiateStreaming(codePromise)
+const codePromise = fetch("https://cdn.tetretalk.com/source/wasm/output.wasm") // fetch the stuff from our cdn
+const { instance } = await WebAssembly.instantiateStreaming(codePromise) // now lets stantiate it
 
-const buffer = new Uint8Array(instance.exports.memory.buffer)
+const buffer = new Uint8Array(instance.exports.memory.buffer) // since its a machine language we gotta convert it to utf8
 
-const pointer = instance.exports.main()
+const pointer = instance.exports.main() // know where to get var from
 
-let string = ""
+let string = "" 
 for(let i = pointer; buffer[i]; i++) {
-  string += String.fromCharCode(buffer[i])
+  string += String.fromCharCode(buffer[i]) // do the stuff for the stuff that does the stuff you know what you mean ugh
 }
 
-document.querySelector("#maintenance > center > a").innerHTML = string;
+document.querySelector("#maintenance > center > a").innerHTML = string; // just boilerplate code lol going to add other stuffs
+// but this is just for testing
 
 })()
 }
